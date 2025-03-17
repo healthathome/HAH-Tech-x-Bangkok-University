4755e9bdbd49ea21e6ffde5fe9ddc7598c18d803
Sitthichai Saejia <tueytoma@users.noreply.github.com>
2025-03-17 22:59:32 +0700

    Update index.js

--- webhook/index.js
+++ webhook/index.js
@@ -16,7 +16,6 @@ const gemini = require('./utils/gemini')
 
 const NodeCache = require('node-cache')
 const cache = new NodeCache()
-const CACHE_IMAGE = 'image_'
 const CACHE_CHAT = 'chat_'
 const port = process.env.PORT || 3000
 
@@ -40,51 +39,55 @@ app.post('/webhook', async (req, res) => {
 					const prompt = event.message.text
 					// console.log('prompt: ', prompt)
 
-					/* 3. Generate text from text-and-image input (multimodal) */
-					// 3.5. Get cache image
-
-					// 3.6. Check available cache
-					// 3.7. Send a prompt to Gemini multimodal
-					// 3.8. Reply a generated text
-
 					/* 1. Generate text from text-only input */
 					// 1.1. Send a prompt to Gemini
+					// const result = await gemini.textOnly(prompt)
 
 					// 1.2. Reply a generated text
+					// await line.reply(event.replyToken, [{
+					// 	type: 'text',
+					// 	text: result,
+					// }])
 
 
 					/* 2. Generate text from text-only input with contextual info */
 					// 2.1. Send a prompt to Gemini
+					// const result = await gemini.textOnlyWithContext(prompt)
 
 					// 2.2. Reply a generated text
+					// await line.reply(event.replyToken, [{
+					// 	type: 'text',
+					// 	text: result,
+					// }])
 
+					/* 3. Build multi-turn conversations (chat) */
+					// 3.1. Get a cache chat history
+					// let chatHistory = cache.get(CACHE_CHAT + userId)
 
-					/* 4. Build multi-turn conversations (chat) */
-					// 4.1. Get a cache chat history
+					// 3.2. Check available cache
+					// if (!chatHistory) {
+					// 	chatHistory = []
+					// }
 
-					// 4.2. Check available cache
+					// 3.3. Send a prompt to Gemini
+					// const result = await gemini.chat(chatHistory, prompt)
 
-					// 4.3. Send a prompt to Gemini
+					// 3.4. Reply a generated text
+					// await line.loading(userId)
+					// await line.reply(event.replyToken, result)
 
-					// 4.4. Reply a generated text
+					// 3.5. Push a new chat history
+					// chatHistory.push({
+					// 	role: 'user',
+					// 	parts: [{ text: prompt }]
+					// })
+					// chatHistory.push({
+					// 	role: 'model',
+					// 	parts: [{ text: result }]
+					// })
 
-					// 4.5. Push a new chat history
-
-					// 4.6. Set a cache chat history
-
-				}
-
-				if (event.message.type === 'image') {
-					/* 3. Generate text from text-and-image input (multimodal) */
-					// 3.1. Get an image binary
-
-					// 3.2. Convert binary to base64
-
-					// 3.3. Set a cache image
-
-					// 3.4. Ask for prompt
-
-					break
+					// 3.6. Set a cache chat history
+					// cache.set(CACHE_CHAT + userId, chatHistory, 60)
 				}
 				break
 		}
